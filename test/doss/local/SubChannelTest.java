2be060e17c503a9e9c4b82a242c50e433a43bc25
Alex Osborne <aosborne@nla.gov.au>
2013-07-15 12:51:28 +1000

    Supress unused warning and run through the autoformatter

--- test/doss/local/SubChannelTest.java
+++ test/doss/local/SubChannelTest.java
@@ -19,148 +19,130 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 
+@SuppressWarnings("unused")
+public class SubChannelTest {
 
-public class SubChannelTest{
-    
-   
     @Rule
     public TemporaryFolder folder = new TemporaryFolder();
     SeekableByteChannel channel1;
     Path testPath;
-    
-    
+
     @After
     public void tearDown() throws Exception {
-      
+
     }
 
-    
     @Before
-    public  void  setUp() throws Exception {
+    public void setUp() throws Exception {
         testPath = FileSystems.getDefault().getPath(folder.newFolder().toPath().toString(), "test");
         writeFileBytes(testPath.toString(), getSomeContent());
     }
-    
-    
-   
-    
+
     @Test
     public void checkReadFromBegining() throws Exception {
 
         try (SeekableByteChannel sbt = Files.newByteChannel(testPath,
                 EnumSet.of(StandardOpenOption.READ))) {
-            
+
             channel1 = new SubChannel(sbt, 0, 25);
             ByteBuffer buffer = ByteBuffer.allocate(15);
             String encoding = System.getProperty("file.encoding");
-            //reads first 15 bytes
+            // reads first 15 bytes
             int r1 = channel1.read(buffer);
             buffer.flip();
             String s1 = Charset.forName(encoding).decode(buffer).toString();
-            assertEquals("Checking that it reads frm the right part of the file ","All the world's" , s1);
+            assertEquals("Checking that it reads frm the right part of the file ", "All the world's", s1);
             buffer.clear();
             int r2 = channel1.read(buffer);
             assertEquals("Checking reading remaining bytes in the subchannel", 10, r2);
-            
+
             buffer.flip();
             String s2 = Charset.forName(encoding).decode(buffer).toString();
-            assertEquals("Checking that remaining bytes read are correct "," a stage,A" , s2);
-           
+            assertEquals("Checking that remaining bytes read are correct ", " a stage,A", s2);
+
             buffer.clear();
-            //can not read any more, all bytes are read
+            // can not read any more, all bytes are read
             int r3 = channel1.read(buffer);
             assertEquals("No more bytes to read, bytest read should be 0", 0, r3);
-           
 
         }
 
-    }  
-    
-    
-    
+    }
+
     @Test
     public void checkReadMiddle() throws Exception {
 
         try (SeekableByteChannel sbt = Files.newByteChannel(testPath,
                 EnumSet.of(StandardOpenOption.READ))) {
-            
+
             channel1 = new SubChannel(sbt, 25, 25);
             ByteBuffer buffer = ByteBuffer.allocate(15);
             String encoding = System.getProperty("file.encoding");
-            //reads first 15 bytes
+            // reads first 15 bytes
             int r1 = channel1.read(buffer);
             buffer.flip();
             String s1 = Charset.forName(encoding).decode(buffer).toString();
-            assertEquals("Checking that it reads frm the right part of the file ","nd all the men " , s1);
+            assertEquals("Checking that it reads frm the right part of the file ", "nd all the men ", s1);
             buffer.clear();
             int r2 = channel1.read(buffer);
             assertEquals("Checking reading remaining bytes in the subchannel", 10, r2);
             buffer.flip();
             String s2 = Charset.forName(encoding).decode(buffer).toString();
-            assertEquals("Checking that remaining bytes read are correct ","and women " , s2);
-           
+            assertEquals("Checking that remaining bytes read are correct ", "and women ", s2);
+
             buffer.clear();
-            //can not read any more, all bytes are read
+            // can not read any more, all bytes are read
             int r3 = channel1.read(buffer);
             assertEquals("No more bytes to read, bytest read should be 0", 0, r3);
-           
 
         }
 
-    }  
-    
-    
-    @Test(expected=IllegalArgumentException.class)
+    }
+
+    @Test(expected = IllegalArgumentException.class)
     public void checkConstructorForIllegalLength() throws Exception {
 
         try (SeekableByteChannel sbt = Files.newByteChannel(testPath,
                 EnumSet.of(StandardOpenOption.READ))) {
-            
+
             channel1 = new SubChannel(sbt, 100, 150);
 
         }
 
-    }  
-    
-    @Test(expected=IllegalArgumentException.class)
+    }
+
+    @Test(expected = IllegalArgumentException.class)
     public void checkConstructorForIllegalOffset() throws Exception {
 
         try (SeekableByteChannel sbt = Files.newByteChannel(testPath,
                 EnumSet.of(StandardOpenOption.READ))) {
-            
+
             channel1 = new SubChannel(sbt, 150, 60);
 
         }
 
-    }  
-    
-    @Test(expected=NullPointerException.class)
+    }
+
+    @Test(expected = NullPointerException.class)
     public void checkConstructorForNullContainerChannel() throws Exception {
 
         try (SeekableByteChannel sbt = Files.newByteChannel(testPath,
                 EnumSet.of(StandardOpenOption.READ))) {
-            
+
             channel1 = new SubChannel(null, 150, 60);
 
         }
 
-    }  
-    
-    
-    
-    
-    
-    
-    
-    
+    }
+
     @Test
     public void testPosition() {
-        
+
         try (SeekableByteChannel sbt = Files.newByteChannel(testPath,
                 EnumSet.of(StandardOpenOption.READ))) {
-            
+
             channel1 = new SubChannel(sbt, 25, 25);
-            SeekableByteChannel sbt1 =  channel1.position(15);
+            SeekableByteChannel sbt1 = channel1.position(15);
             ByteBuffer buffer = ByteBuffer.allocate(7);
             String encoding = System.getProperty("file.encoding");
             int r1 = channel1.read(buffer);
@@ -168,88 +150,75 @@ public class SubChannelTest{
             String s1 = Charset.forName(encoding).decode(buffer).toString();
             System.out.println(s1);
             assertEquals("Checking if we are reading from the correct position ", "and wom", s1);
-            
-  
 
-        }catch(IOException ioe){
+        } catch (IOException ioe) {
             fail(ioe.getClass().getName() + " not expected");
         }
-        
+
     }
 
-    
-    @Test(expected=IllegalArgumentException.class)
+    @Test(expected = IllegalArgumentException.class)
     public void testPositionBeforeSubChannelStart() {
-        
+
         try (SeekableByteChannel sbt = Files.newByteChannel(testPath,
                 EnumSet.of(StandardOpenOption.READ))) {
-            
-            channel1 = new SubChannel(sbt, 25, 25);
-            SeekableByteChannel sbt1 =  channel1.position(-20);
-           
-  
 
-        }catch(IOException ioe){
+            channel1 = new SubChannel(sbt, 25, 25);
+            SeekableByteChannel sbt1 = channel1.position(-20);
+
+        } catch (IOException ioe) {
             fail("IOException not expected");
         }
-        
+
     }
-    
-    @Test(expected=IllegalArgumentException.class)
+
+    @Test(expected = IllegalArgumentException.class)
     public void testPositionAfterChannelEnd() {
-        
+
         try (SeekableByteChannel sbt = Files.newByteChannel(testPath,
                 EnumSet.of(StandardOpenOption.READ))) {
-            
-            channel1 = new SubChannel(sbt, 25, 25);
-            SeekableByteChannel sbt1 =  channel1.position(52);
-           
-  
 
-        }catch(IOException ioe){
+            channel1 = new SubChannel(sbt, 25, 25);
+            SeekableByteChannel sbt1 = channel1.position(52);
+
+        } catch (IOException ioe) {
             fail("IOException not expected");
         }
-        
+
     }
-    
-    
- 
-    
-    @Test(expected=NonWritableChannelException.class)
-    public void testWrite() throws IOException{
-        
-        try ( SeekableByteChannel sbt = Files.newByteChannel(testPath, EnumSet.of(StandardOpenOption.READ)))
+
+    @Test(expected = NonWritableChannelException.class)
+    public void testWrite() throws IOException {
+
+        try (SeekableByteChannel sbt = Files.newByteChannel(testPath, EnumSet.of(StandardOpenOption.READ)))
         {
-        channel1 = new SubChannel(sbt, 1, 25 );
-    	String s = "traralaah";
-    	 final byte[] bytes = s.getBytes();
-    	channel1.write(ByteBuffer.wrap(bytes));
+            channel1 = new SubChannel(sbt, 1, 25);
+            String s = "traralaah";
+            final byte[] bytes = s.getBytes();
+            channel1.write(ByteBuffer.wrap(bytes));
         }
     }
-    
-    
-    @Test(expected=NonWritableChannelException.class)
-    public void tesTruncatee() throws IOException{
-        
-        try ( SeekableByteChannel sbt = Files.newByteChannel(testPath, EnumSet.of(StandardOpenOption.READ)))
+
+    @Test(expected = NonWritableChannelException.class)
+    public void tesTruncatee() throws IOException {
+
+        try (SeekableByteChannel sbt = Files.newByteChannel(testPath, EnumSet.of(StandardOpenOption.READ)))
         {
-            channel1 = new SubChannel(sbt, 1, 25 );
+            channel1 = new SubChannel(sbt, 1, 25);
             channel1.truncate(5);
         }
     }
-    
-    
-   private  static void writeFileBytes(String filename, String content) {
+
+    private static void writeFileBytes(String filename, String content) {
         try {
-            Files.write( FileSystems.getDefault().getPath( filename),
-                         content.getBytes(),
-                         StandardOpenOption.CREATE);
-        }
-        catch ( IOException ioe ) {
+            Files.write(FileSystems.getDefault().getPath(filename),
+                    content.getBytes(),
+                    StandardOpenOption.CREATE);
+        } catch (IOException ioe) {
             ioe.printStackTrace();
         }
     }
-   
+
     private static String getSomeContent() {
         String content = "All the world's a stage,"
                 + "And all the men and women merely players;"
