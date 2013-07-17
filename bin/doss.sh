7e696a999f989b576b2b57928e2a3a62aa23e77d
JonathanShaw <anaximenes@gmail.com>
2013-07-17 12:17:52 +1000

    use rm -f

--- bin/doss.sh
+++ bin/doss.sh
@@ -2,7 +2,7 @@
 cd "`dirname \"$0\"`/.."
 if  [ -d "target/classes" ]
 then
-    [ pom.xml -nt .javaclasspath ] && rm .javaclasspath && mvn dependency:build-classpath -q -Dmdep.outputFile=.javaclasspath
+    [ pom.xml -nt .javaclasspath ] && rm -f .javaclasspath && mvn dependency:build-classpath -q -Dmdep.outputFile=.javaclasspath
     [ ! -d /tmp/1 ] && mkdir /tmp/1  
     java -Ddoss.home=/tmp/1  -cp "$(cat .javaclasspath):target/classes" doss.Main $@
 else
