d831d8c7255ceef4c12a81bfe6492678b780857a
JonathanShaw <anaximenes@gmail.com>
2013-07-17 12:19:45 +1000

    ensure arguments are passed unchanged to doss.main

--- bin/doss.sh
+++ bin/doss.sh
@@ -4,7 +4,7 @@ if  [ -d "target/classes" ]
 then
     [ pom.xml -nt .javaclasspath ] && rm -f .javaclasspath && mvn dependency:build-classpath -q -Dmdep.outputFile=.javaclasspath
     [ ! -d /tmp/1 ] && mkdir /tmp/1  
-    java -Ddoss.home=/tmp/1  -cp "$(cat .javaclasspath):target/classes" doss.Main $@
+    java -Ddoss.home=/tmp/1  -cp "$(cat .javaclasspath):target/classes" doss.Main "$@"
 else
   echo "It looks like you have not compiled Doss yet."
   echo "Try running mvn clean compile"
