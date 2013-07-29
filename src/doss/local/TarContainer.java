4f045a8d9ae7daa7bd1e1e0604c254eea287bfd4
snezanam <smihajlo@nla.gov.au>
2013-07-30 09:27:43 +1000

    Added constant TAR_ENTRY_HEADER_LENGTH

--- src/doss/local/TarContainer.java
+++ src/doss/local/TarContainer.java
@@ -25,6 +25,8 @@ public class TarContainer implements Container {
     Path path;
     final private long id;
     SeekableByteChannel containerChannel;
+    ArchiveOutputStream tarOut;
+    private static final int TAR_ENTRY_HEADER_LENGTH = 512;
 
     TarContainer(long id, Path path) throws IOException, ArchiveException {
 
@@ -39,7 +41,11 @@ public class TarContainer implements Container {
     }
 
     public void close() {
+
         try {
+            if (tarOut != null) {
+                tarOut.finish();
+            }
             containerChannel.close();
         } catch (IOException e) {
             throw new TarContainerException("Unable to close tar container "
@@ -49,15 +55,15 @@ public class TarContainer implements Container {
 
     public Blob get(long offset) throws IOException {
 
-        if (offset < 512) {
+        if (offset < TAR_ENTRY_HEADER_LENGTH) {
             throw new IllegalArgumentException(
                     "Tar offset can not be less than 512");
         }
         TarBlob tarBlob = null;
 
-        SubChannel subChannel = new SubChannel(containerChannel, offset - 512,
-                512);
-        ByteBuffer byteBuffer = ByteBuffer.allocate(512);
+        SubChannel subChannel = new SubChannel(containerChannel, offset
+                - TAR_ENTRY_HEADER_LENGTH, 512);
+        ByteBuffer byteBuffer = ByteBuffer.allocate(TAR_ENTRY_HEADER_LENGTH);
         subChannel.read(byteBuffer);
         byteBuffer.flip();
         TarArchiveEntry entry = new TarArchiveEntry(byteBuffer.array());
@@ -70,7 +76,7 @@ public class TarContainer implements Container {
         Long offset = 0L;
 
         TarArchiveOutputStream os = null;
-        ArchiveOutputStream tarOut = null;
+        tarOut = null;
         FileOutputStream outputStream = null;
         if (containerChannel.size() == 0) {
             try {
@@ -83,7 +89,7 @@ public class TarContainer implements Container {
                 tarOut.putArchiveEntry(entry);
                 output.writeTo(Channels.newChannel(tarOut));
 
-                offset = 512L;
+                offset = new Integer(TAR_ENTRY_HEADER_LENGTH).longValue();
 
             } catch (ArchiveException e) {
                 throw new TarContainerException(
@@ -94,7 +100,6 @@ public class TarContainer implements Container {
                 tarOut.close();
 
                 tarOut.flush();
-                // tarOut.finish();
 
             }
 
@@ -104,9 +109,9 @@ public class TarContainer implements Container {
             TarArchiveEntry entry = new TarArchiveEntry(String.valueOf(id));
             entry.setSize(output.size());
 
-            byte[] entryData = new byte[512];
+            byte[] entryData = new byte[TAR_ENTRY_HEADER_LENGTH];
 
-            byte[] endTarDta = new byte[1024];
+            byte[] endTarDta = new byte[2 * (TAR_ENTRY_HEADER_LENGTH)];
             ByteBuffer endByteByffer = ByteBuffer.wrap(endTarDta);
 
             entry.writeEntryHeader(entryData);
@@ -114,6 +119,7 @@ public class TarContainer implements Container {
 
             containerChannel.position(pos);
             containerChannel.write(bb);
+
             output.writeTo(containerChannel);
 
             endByteByffer.flip();
@@ -131,29 +137,28 @@ public class TarContainer implements Container {
     public long getPosition() throws IOException {
         long offset = 0L;
         long p = -512;
-        byte[] emptyBlock = new byte[512];
+        byte[] emptyBlock = new byte[TAR_ENTRY_HEADER_LENGTH];
         boolean condition = true;
-        byte[] bytes = new byte[512];
+        byte[] bytes = new byte[TAR_ENTRY_HEADER_LENGTH];
         long position = 0L;
-        try (SeekableByteChannel channel = Files.newByteChannel(path,
-                EnumSet.of(StandardOpenOption.READ))) {
-            while (condition) {
 
-                p = p + 512;
-                channel.position(p);
+        while (condition) {
 
-                ByteBuffer bb = ByteBuffer.wrap(bytes);
-                offset = channel.read(bb);
-                bb.flip();
-                if (Arrays.equals(bb.array(), emptyBlock)) {
-                    condition = false;
-                }
-                bb.clear();
+            p = p + TAR_ENTRY_HEADER_LENGTH;
+            containerChannel.position(p);
 
+            ByteBuffer bb = ByteBuffer.wrap(bytes);
+            offset = containerChannel.read(bb);
+            bb.flip();
+            if (Arrays.equals(bb.array(), emptyBlock)) {
+                condition = false;
             }
+            bb.clear();
 
-            position = channel.position() - 512;
         }
+
+        position = containerChannel.position() - TAR_ENTRY_HEADER_LENGTH;
+
         return position;
     }
 
